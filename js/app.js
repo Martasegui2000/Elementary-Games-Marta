35116bb683e2a000170ec9ec7d3528b02dda4eb0
Marta <msg00@iesemilidarder.com>
2020-10-15 13:09:59 +0200

    Modificaciones en el html y creación del app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// To load lGames with example Data
+
+lGames.push(new Gamepad(1, "Star Wars Jedi: Fallen Order", "Five Start Studio", "12/12/2019", "Action", "16"));
+lGames.push(new Gamepad(2, "Terminator", "Marvel Studio", "12/12/2019", "Adventure", "12"));
+lGames.push(new Gamepad(3, "Halo", "Pixel Studio", "12/12/2019", "Rol", "18"));
+
